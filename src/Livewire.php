1fac1d78969f216c03d65bee0a011d099786d11c
Alexandre Basso <kaxias@gmail.com>
2020-04-29 18:23:14 +0100

    Update Livewire.php

--- src/Livewire.php
+++ src/Livewire.php
@@ -5,13 +5,29 @@ namespace Livewire;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void component($alias, $viewClass)
- * @method static \Livewire\Testing\TestableLivewire test($name, $params = [])
+ * @method static \Livewire\LivewireManager activate($component, $id)
+ * @method static \Livewire\LivewireManager component($alias, $viewClass)
+ * @method static \Livewire\LivewireManager dispatch($event, ...$params)
+ * @method static \Livewire\LivewireManager listen($event, $callback)
+ * @method static \Livewire\LivewireManager isLaravel7()
+ * @method static \Livewire\LivewireManager registerHydrationMiddleware(array $classes)
+ * @method static \Livewire\LivewireManager registerInitialHydrationMiddleware(array $callables)
+ * @method static \Livewire\LivewireManager registerInitialDehydrationMiddleware(array $callables)
+ * @method static \Livewire\LivewireManager scripts($options = [])
+ * @method static \Livewire\LivewireManager styles($options = [])
+ * @method static \Livewire\LivewireManager hydrate($instance, $request)
+ * @method static \Livewire\LivewireManager dehydrate($instance, $request)
+ * @method static \Livewire\LivewireManager test($name, $params = [])
  *
  * @see \Livewire\LivewireManager
  */
 class Livewire extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     public static function getFacadeAccessor()
     {
         return 'livewire';
